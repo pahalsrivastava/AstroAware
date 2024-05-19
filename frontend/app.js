6f3638effbaa52a0e4367e240fe8cd8fe2b57b19
Pahal Srivastava <149412490+pahalsrivastava@users.noreply.github.com>
2024-05-19 17:56:00 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import EventScreen from './screens/EventScreen';
+import WeatherScreen from './screens/WeatherScreen';
+import ImageProcessingScreen from './screens/ImageProcessingScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Events" component={EventScreen} />
+        <Tab.Screen name="Space Weather" component={WeatherScreen} />
+        <Tab.Screen name="Image Processing" component={ImageProcessingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
