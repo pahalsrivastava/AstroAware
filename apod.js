2825122bba01e1893f723e4252cea042e1b28e08
Pahal Srivastava <149412490+pahalsrivastava@users.noreply.github.com>
2024-07-15 22:25:31 +0530

    Update apod.js with new API key

--- apod.js
+++ apod.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = 'YOUR_NASA_API_KEY';  // Replace with your NASA API key
-    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = "RRiEhLzmnj6KbDfgc0E1OcOShB70BNtiwR2wXJfV";
+    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${RRiEhLzmnj6KbDfgc0E1OcOShB70BNtiwR2wXJfV}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('apod-image').src = data.url;
-            document.getElementById('apod-title').textContent = data.title;
-            document.getElementById('apod-description').textContent = data.explanation;
+            document.getElementById("apod-image").src = data.url;
+            document.getElementById("apod-title").textContent = data.title;
+            document.getElementById("apod-description").textContent = data.explanation;
         })
-        .catch(error => console.error('Error fetching data:', error));
+        .catch(error => console.error("Error fetching APOD:", error));
 });
